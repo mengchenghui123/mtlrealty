05e84afe6dbe54ff64d5185047db906fe3f05f16
BaiyangChen <boyang96@yahoo.ca>
2024-07-03 17:07:29 -0400

    added slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Residencies from './components/Residencies/Risidencies'
+import './App.css'
+
+
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <Hero/>
+    <div className="App">
+      <div>
+        <div className="white-gradient">
+          <Header/>
+          <Hero/>
+          <Residencies/>
+      </div>
+      
+      </div>
     </div>
   );
 }
