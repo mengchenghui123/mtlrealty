6ab98cb9355cd462f8dc04a8a4552025abe935f4
BaiyangChen <boyang96@yahoo.ca>
2024-09-27 16:57:15 -0400

    test8

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "https://mtlrealty-two.vercel.app/api",
+  baseURL: "https://mtlrealty-server.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
