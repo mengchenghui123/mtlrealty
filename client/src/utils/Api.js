4407d617af3a7421fb1a08214068add2535a75e6
BaiyangChen <boyang96@yahoo.ca>
2024-10-09 15:32:57 -0400

    Update Api.js

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://mtlrealty.vercel.app/api",
+  baseURL: "https://real-estate-server-gamma.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
